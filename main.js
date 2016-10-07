7a8bb7f793dbd68f3f74dbc46024e9b494f2a974
Maxwell Russell <maxruss.87@gmail.com>
2016-10-07 15:26:05 -0500

    table generator added

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
-  // undefined
-  // null
-  // true
-  // false
-  // ""
-  // "1.2"
-  // "one"
-  // 0
-  // -0
-  // NaN
-  // Infinity
-  // -Infinity
-  // 1
-  // {}
-  // []
-  // [12]
-  // ['something']
-  // function(){}
+var foo = [undefined , null , true , false , "" , "1.2", "one" , 0 , -0 , NaN , Infinity , -Infinity , 1 , {} , [] , [12] , ['something']];
+
+document.write('<table>');
+document.write('<tr><th>Origin</th><th>Number Coercion</th><th>String Coercion</th><th>Boolean Coercion</th></tr>');
+for (x=0; x <= 10; x++) {
+      document.write('<tr><td>' + foo[x] + '</td><td>' + Number(foo[x]) + '</td><td>' + String(foo[x]) + '</td><td>' + Boolean(foo[x]) + '</td></tr>');
+};
+document.write('</table>');
