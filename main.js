fd07d1f7df7a0282702687d161173438a424a789
Maxwell Russell <maxruss.87@gmail.com>
2016-10-07 18:37:42 -0500

    final adjustments made to table

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-var foo = [undefined , null , true , false , "" , "1.2", "one" , 0 , -0 , NaN , Infinity , -Infinity , 1 , {} , [] , [12] , ['something']];
+function hello() {
+  console.log("Hello world!");
+}
+
+var foo = [undefined , null , true , false , "" , "1.2", "one" , 0 , -0 , NaN , Infinity , -Infinity , 1 , {} , [] , [12] , ['something'], function(){}];
+
+var bar = ['undefined' , 'null' , 'true' , 'false' , '""' , '"1.2"', '"one"' , '0' , '-0' , 'NaN' , "Infinity" , "-Infinity" , "1" , "{}" , "[]" , '[12]' , "['something']" , 'function(){}'];
 
 document.write('<table>');
 document.write('<tr><th>Origin</th><th>Number Coercion</th><th>String Coercion</th><th>Boolean Coercion</th></tr>');
-for (x=0; x <= 10; x++) {
-      document.write('<tr><td>' + foo[x] + '</td><td>' + Number(foo[x]) + '</td><td>' + String(foo[x]) + '</td><td>' + Boolean(foo[x]) + '</td></tr>');
+for (x=0; x < foo.length; x++) {
+      document.write('<tr><td>' + bar[x] + '</td><td>' + Number(foo[x]) + '</td><td>' + String(foo[x]) + '</td><td>' + Boolean(foo[x]) + '</td></tr>');
+      console.log(foo[x]);
 };
 document.write('</table>');
